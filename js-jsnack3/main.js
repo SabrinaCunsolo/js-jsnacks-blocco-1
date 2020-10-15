1cf8a5f350716d786e1e4d6355ea77e34d5de6a5
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-15 16:05:58 +0200

    array

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -2,15 +2,21 @@
 
  // chiedo 5 volte all'utente 5 numeri
 var numero = parseInt (prompt("Scrivi un numero"));
+var array = [numero];
 // var numero2 = parseInt(prompt("Scrivi un altro numero"));
 // var numero3 = parseInt(prompt("Scrivi un altro numero"));
 // var numero4 = parseInt(prompt("Scrivi un altro numero"));
 // var numero5 = parseInt(prompt("Scrivi un altro numero"));
 
-console.log("somma =" + numero1 + numero2 + numero3 + numero4 + numero5);
+// console.log("somma =" + numero1 + numero2 + numero3 + numero4 + numero5);
 
 // devo sommare tutti i numeri inseriti
-var somma = + numero1 + numero2 + numero3 + numero4 + numero5;
+// var somma = + numero1 + numero2 + numero3 + numero4 + numero5;
+for (var i = 0; i < array.length; i++) {
+    array[i]
+}
+
+console.log(array[i]);
 
 // devo stampare la somma
 document.writeln("somma =" + somma);
