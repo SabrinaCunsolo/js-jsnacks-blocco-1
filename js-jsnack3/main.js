3fd395521fa4b6eba24cd7ea58d9bd46881780e4
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-15 16:39:16 +0200

    esercizio 3

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -1,22 +1,15 @@
  // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 
  // chiedo 5 volte all'utente 5 numeri
-var numero = parseInt (prompt("Scrivi un numero"));
-var array = [numero];
-// var numero2 = parseInt(prompt("Scrivi un altro numero"));
-// var numero3 = parseInt(prompt("Scrivi un altro numero"));
-// var numero4 = parseInt(prompt("Scrivi un altro numero"));
-// var numero5 = parseInt(prompt("Scrivi un altro numero"));
+var somma = 0;
 
-// console.log("somma =" + numero1 + numero2 + numero3 + numero4 + numero5);
+ for (var i = 0; i < 5; i++) {
+    var numero = parseInt (prompt("Scrivi un numero"));
+    // console.log(numero);
+    somma = (numero + somma);
+    // somma += numero è la stessa cosa di sopra
+ }
 
-// devo sommare tutti i numeri inseriti
-// var somma = + numero1 + numero2 + numero3 + numero4 + numero5;
-for (var i = 0; i < array.length; i++) {
-    array[i]
-}
 
-console.log(array[i]);
-
-// devo stampare la somma
+// // devo stampare la somma
 document.writeln("somma =" + somma);
