d201a8f729c5845d6d4959ecdce5bd2abfeed75e
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-15 14:57:15 +0200

    fine esercizio snack3

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -1,6 +1,16 @@
  // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+ // chiedo 5 volte all'utente 5 numeri
 var numero1 = parseInt(prompt("Scrivi un numero"));
 var numero2 = parseInt(prompt("Scrivi un altro numero"));
 var numero3 = parseInt(prompt("Scrivi un altro numero"));
 var numero4 = parseInt(prompt("Scrivi un altro numero"));
 var numero5 = parseInt(prompt("Scrivi un altro numero"));
+
+console.log("somma =" + numero1 + numero2 + numero3 + numero4 + numero5);
+
+// devo sommare tutti i numeri inseriti
+var somma = + numero1 + numero2 + numero3 + numero4 + numero5;
+
+// devo stampare la somma
+document.writeln("somma =" + somma);
